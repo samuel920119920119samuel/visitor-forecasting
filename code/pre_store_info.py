c96cca0e56fbb48123cbc49d8b8eb8738b63c173
neera <neeralin@gmail.com>
2018-12-14 18:10:40 +0800

    store info(no weather)

---
+++ code/pre_store_info.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn import preprocessing
+
+data = {
+    'si': pd.read_csv('../data/air_store_info.csv')
+}
+
+lbl = preprocessing.LabelEncoder()
+data['si']['air_genre_name'] = lbl.fit_transform(data['si']['air_genre_name'])
+data['si']['air_area_name'] = lbl.fit_transform(data['si']['air_area_name'])
+
+data['si'].to_pickle("store_info.pkl")
