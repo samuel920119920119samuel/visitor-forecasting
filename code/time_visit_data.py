accff5738e320ca7f5742fbf4e793fea9f5c2070
XinMiaoWang <45872222+XinMiaoWang@users.noreply.github.com>
2018-12-16 22:58:16 +0800

    Add files via upload
    dof轉成數字
    visit_date->to_datetime()

--- code/time_visit_data.py
+++ code/time_visit_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import calendar
 from datetime import datetime
-
+from sklearn import preprocessing
 
 def get_day_of_week(dateString):
     day_of_week = datetime.strptime(dateString, "%Y-%m-%d").weekday()
@@ -17,6 +17,8 @@ def get_year(dateString):
 def get_week_of_year(dateString):
     return (datetime.strptime(dateString, "%Y-%m-%d").strftime("%W"))
 
+def string_to_date(dateString):
+    return datetime.strptime(dateString, "%Y-%m-%d")
 
 data = {
     'tra': pd.read_csv('../data/air_visit_data.csv')
@@ -26,5 +28,10 @@ data['tra']['year'] = data['tra']['visit_date'].apply(get_year)
 data['tra']['month'] = data['tra']['visit_date'].apply(get_month)
 data['tra']['day_of_week'] = data['tra']['visit_date'].apply(get_day_of_week)
 data['tra']['week_of_year'] = data['tra']['visit_date'].apply(get_week_of_year)
+data['tra']['visit_date'] = data['tra']['visit_date'].apply(string_to_date)
+
+lbl = preprocessing.LabelEncoder()
+data['tra']['day_of_week'] = lbl.fit_transform(data['tra']['day_of_week'])
 
 data['tra'].to_pickle('visit_data.pkl')
+
