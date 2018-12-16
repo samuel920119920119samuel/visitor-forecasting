33b4130734b78a4a55adafbc944510ae3fd94d1d
XinMiaoWang <45872222+XinMiaoWang@users.noreply.github.com>
2018-12-16 23:41:25 +0800

    Add files via upload

--- code/time_visit_data.py
+++ code/time_visit_data.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
-import calendar
 from datetime import datetime
-from sklearn import preprocessing
 
 def get_day_of_week(dateString):
-    day_of_week = datetime.strptime(dateString, "%Y-%m-%d").weekday()
-    return(calendar.day_name[day_of_week])
+    return (datetime.strptime(dateString, "%Y-%m-%d").weekday())
 
 def get_month(dateString):
     return(datetime.strptime(dateString, "%Y-%m-%d").month)
@@ -30,8 +27,4 @@ data['tra']['day_of_week'] = data['tra']['visit_date'].apply(get_day_of_week)
 data['tra']['week_of_year'] = data['tra']['visit_date'].apply(get_week_of_year)
 data['tra']['visit_date'] = data['tra']['visit_date'].apply(string_to_date)
 
-lbl = preprocessing.LabelEncoder()
-data['tra']['day_of_week'] = lbl.fit_transform(data['tra']['day_of_week'])
-
 data['tra'].to_pickle('visit_data.pkl')
-
